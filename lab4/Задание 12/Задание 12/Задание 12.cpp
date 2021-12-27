83330d5769b080dfae6bdbb70c6d0369b9729723
Carcajo <t375445391507@gmail.com>
2021-12-27 20:27:12 +0300

    append labs 5-6-7-8

---
+++ lab4/Задание 12/Задание 12/Задание 12.cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+
+// Ввод целочисленного значения (число больше 0)
+long long int inputInt() {
+    long double value;
+
+    std::cin >> value;
+
+    // Удаление всех пробелов (если они есть) после первого числа, если оно было в потоке
+    while (std::cin.peek() == ' ')
+        std::cin.ignore(1, ' ');
+
+    // Проверка состояния потока ввода, конца строки, области допустимых значений и экспоненты
+    while (std::cin.fail() || std::cin.peek() != '\n' || value <= 0 || value != (long long int)value) {
+        std::cout << "Ввод некорректен. Введите целое положительное числовое значение (до 2⁶³): ";
+
+        std::cin.clear(); // Включение потока ввода
+        std::cin.ignore(3256, '\n'); // Очищение буфера потока ввода
+
+        std::cin >> value;
+
+        while (std::cin.peek() == ' ')
+            std::cin.ignore(1, ' ');
+    }
+
+    return value;
+}
+
+double inputDouble(long long int i2) {
+    double value;
+
+    std::cin >> value;
+
+    while (std::cin.fail()) {
+        std::cout << "Ввод некорректен. Введите элемент [" << i2 + 1 << "] заново:\n";
+
+        std::cin.clear();
+        std::cin.ignore(3256, '\n');
+
+        std::cin >> value;
+    }
+
+    return value;
+}
+
+void deleteDoub(int n, double* arr) {
+    for (long long int i = 1; i < n; ++i) {
+        for (long long j = i - 1; j > -1; --j) {
+            if (arr[i] == arr[j]) {
+                for (long long z = i; z < n - 1; ++z) {
+                    arr[z] = arr[z + 1];
+                }
+                --n;
+                --i;
+                break;
+            }
+        }
+    }
+
+    std::cout << "\n\nПреобразованный массив:\n";
+    for (long long int i = 0; i < n; ++i) {
+        std::cout << arr[i] << "  ";
+    }
+    std::cout << "\n\nНовый размер массива: " << n << '.';
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int n;
+
+    std::cout << "Программа удаляет дубликаты элементов из массива.\n\n";
+    std::cout << "Введите размер массива: ";
+    n = inputInt();
+
+    double* arr = new double[n];
+
+    std::cout << "Введите элементы массива: ";
+    for (long long int i = 0; i < n; ++i) {
+        arr[i] = inputDouble(i);
+    }
+
+    deleteDoub(n, arr); // Функция удаления дубликатов в массиве
+
+    delete[] arr;
+
+    return 0;
+}
---
+++ lab4/задание 4/задание 4/задание 4.cpp
@@ -0,0 +1,65 @@
+﻿#include <iostream>
+using namespace std;
+void read(int& a)
+{
+	while (true)
+	{
+		cin >> a;
+		if (cin.fail() || cin.peek() != '\n')
+		{
+			cout << "Ошибка! Неверный ввод\n";
+			cin.clear();
+			cin.ignore(32767, '\n');
+		}
+		else
+			break;
+	}
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int N, B, ind(0);
+	cout << "Введите количество строк: ";
+	do  read(N);  while (N < 1);
+	cout << "Введите количество сстолбцов: ";
+	do  read(B);  while (B < 1);
+	int** arr = new int* [N];
+	for (int i(0); i < N; i++)
+	{
+		arr[i] = new int[N];
+	}
+
+	for (int i(0); i < N; i++)
+	{
+		for (int j(0); j < B; j++)
+		{
+			cout << "Введите элемент матрицы [" << i + 1 << "][" << j + 1 << "]: ";
+			read(arr[i][j]);
+		}
+	}
+	cout << "Массив:\n";
+	for (int i(0); i < N; i++)
+	{
+		for (int j(0); j < B; j++)
+			cout << arr[i][j] << " ";
+		cout << endl;
+	}
+	for (int i(0); i < N; i++)
+	{
+		for (int j(0); j < B; j++)
+		{
+			if ((arr[i][j] < arr[abs(i - 1)][j]) && (arr[i][j] < arr[((i + 1) == N ? i : i + 1)][j]) &&
+				(arr[i][j] < arr[i][((j + 1) == B ? j : j + 1)]) && (arr[i][j] < arr[i][abs(j - 1)]) &&
+				(arr[i][j] < arr[((i + 1) == N ? i : i + 1)][abs(j - 1)]) && (arr[i][j] < arr[((i + 1) == N ? i : i + 1)][((j + 1) == B ? j : j + 1)]) &&
+				(arr[i][j] < arr[abs(i - 1)][abs(j - 1)]) && (arr[i][j] < arr[abs(i - 1)][((j + 1) == B ? j : j + 1)])) ind++;
+		}
+	}
+	cout << "Количество локальных минимумов: " << ind;
+	{
+	for (int i(0); i < N; i++)
+		delete[] arr[i];
+	}
+	delete[] arr;
+	return 0;
+}
